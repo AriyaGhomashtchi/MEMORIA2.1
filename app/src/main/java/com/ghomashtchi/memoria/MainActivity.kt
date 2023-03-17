bd5d860c33ab2a73387b45226a92399cf7905292
ariyaghomashtchi <ariya_ghomashtchi@yahoo.com>
2023-03-17 14:25:22 +0100

    current stand.

--- app/src/main/java/com/ghomashtchi/memoria/MainActivity.kt
+++ app/src/main/java/com/ghomashtchi/memoria/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
         } else {
             window.setFlags(
--- app/src/main/java/com/ghomashtchi/memoria/ui/LogIn_fragment.kt
+++ app/src/main/java/com/ghomashtchi/memoria/ui/LogIn_fragment.kt
@@ -5,29 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.ghomashtchi.memoria.ARG_PARAM1
-import com.ghomashtchi.memoria.ARG_PARAM2
+import androidx.navigation.Navigation
 import com.ghomashtchi.memoria.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LogIn_fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LogIn_fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
-    override fun onCreateView(
+    fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
@@ -35,23 +23,4 @@ class LogIn_fragment : Fragment() {
         return inflater.inflate(R.layout.fragment_log_in, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LogIn_fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-}
+
