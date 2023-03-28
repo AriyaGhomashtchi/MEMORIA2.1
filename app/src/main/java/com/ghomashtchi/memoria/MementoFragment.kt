1bcf8588e3d3afa21506818bc724ad3fe6c7ff1e
ariyaghomashtchi <ariya_ghomashtchi@yahoo.com>
2023-03-28 16:38:04 +0200

    current stand. Working on the bindings and the ClickListeners.

--- app/src/main/java/com/ghomashtchi/memoria/MementoFragment.kt
+++ app/src/main/java/com/ghomashtchi/memoria/MementoFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ghomashtchi.memoria.databinding.FragmentMementoBinding
 
 class MementoFragment : Fragment() {
+
+    private var _binding: FragmentMementoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_memento, container, false)
     }
 }
